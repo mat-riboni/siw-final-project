c5d409be6f60ea1fc0a4134941dea3b8d3f3a314
Mattia Riboni <154544827+mat-riboni@users.noreply.github.com>
2024-05-18 17:59:21 +0200

    added id in credentials

--- src/main/java/it/uniroma3/vestiti/controller/NegozioController.java
+++ src/main/java/it/uniroma3/vestiti/controller/NegozioController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import it.uniroma3.vestiti.model.Negozio;
 import it.uniroma3.vestiti.service.NegozioService;
 
 @Controller
@@ -14,10 +17,16 @@ public class NegozioController {
 	@Autowired
 	NegozioService negozioService;
 	
-	//il pah e' da cambiare in 'negoziante/nuovoNegozio' e aggiornare anche authconfig
+	//il path e' da cambiare in 'negoziante/nuovoNegozio' e aggiornare anche authconfig
 		@GetMapping("/nuovoNegozio")
-		public String nuovoNegozio(Model model) {
-			
+		public String getNuovoNegozioForm(Model model) {
+			model.addAttribute("negozio", new Negozio());
 			return "nuovoNegozioForm.html";
+		}
+		
+		//il path e' da cambiare in 'negoziante/nuovoNegozio' e aggiornare anche authconfig
+		@PostMapping("/nuovoNegozio")
+		public String InserisciNuovoNegozio(@ModelAttribute("negozio") Negozio negozio) {
+			return "redirect:/";
 		}
 }
---
+++ src/main/resources/script/nuovoNegozio.js
@@ -0,0 +1,5 @@
+function appendNuovaCategoria(){
+	document.getElementById("categorie-container").innerHTML += '<input class="uk-input nuova-categoria" type="text" id="descizione" placeholder="Categoria">'
+}
+
+
